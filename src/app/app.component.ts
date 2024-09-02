7c5fae585c7a46f6b43217a15e9b71cc68c1554e
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-02 21:20:52 +0200

    import components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LekserMaaGjoresComponent } from './lekser-maa-gjores/lekser-maa-gjores.component';
+import { LekserGjortComponent } from './lekser-gjort/lekser-gjort.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LekserMaaGjoresComponent, LekserGjortComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
