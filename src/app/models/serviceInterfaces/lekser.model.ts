0a91e3ed9bf7ce0a8aa713da499d435ab3823dcc
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-02 20:40:53 +0200

    Set id's type to number

--- src/app/models/serviceInterfaces/lekser.model.ts
+++ src/app/models/serviceInterfaces/lekser.model.ts
@@ -3,9 +3,9 @@ import { Lekse } from "../lekse";
 export interface ILekseService {
     Laglekse(lekse: Lekse): void;
     HentAlle(): Lekse[];
-    Hent(id: string): Lekse;
+    Hent(id: number): Lekse;
     HentUferdige(): Lekse[];
     HentFullforte(): Lekse[];
-    MarkerFullfort(id: string): void;
+    MarkerFullfort(id: number): void;
     OppdaterLekse(lekse: Lekse): void;
 }
