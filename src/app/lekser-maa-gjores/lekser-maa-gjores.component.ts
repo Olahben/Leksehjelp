30a47a42eb0efa28183e8bd4c8d59ae93e16b8ee
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-06 15:48:32 +0200

    Add functionality and UI to add homework

--- src/app/lekser-maa-gjores/lekser-maa-gjores.component.ts
+++ src/app/lekser-maa-gjores/lekser-maa-gjores.component.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
+import { Lekse } from '../models/lekse';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LekserService } from '../services/lekser.service';
 
 @Component({
   selector: 'app-lekser-maa-gjores',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './lekser-maa-gjores.component.html',
   styleUrl: './lekser-maa-gjores.component.scss'
 })
 export class LekserMaaGjoresComponent {
+  homeworkForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private lekserService: LekserService) {
+    this.homeworkForm = this.fb.group({
+      fag: ['', Validators.required],
+      innleveringsdato: ['', Validators.required],
+      beskrivelse: ['', Validators.required]
+    });
+  }
+
   ShowModal() {
     const element = document.getElementById("add-homework") as HTMLDialogElement;
     element.showModal();
+  }
+
+  closeModal() {
+    const element = document.getElementById("add-homework") as HTMLDialogElement;
+    element.close();
+  }
+
+  onSubmit(): void {
+    if (this.homeworkForm.valid) {
+      const homeworkData = this.homeworkForm.value;
+      this.lekserService.Laglekse(homeworkData.fag, homeworkData.innleveringsDato, homeworkData.beskrivelse);
+      console.log("success");
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
 
