a972940d525e746b651879ededf58c8a7ef6e719
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-07 15:34:43 +0200

    Display homework that is done

--- src/app/lekser-gjort/lekser-gjort.component.ts
+++ src/app/lekser-gjort/lekser-gjort.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { LekserService } from '../services/lekser.service';
+import { Lekse } from '../models/lekse';
+import { NgFor, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lekser-gjort',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CommonModule],
   templateUrl: './lekser-gjort.component.html',
   styleUrl: './lekser-gjort.component.scss'
 })
-export class LekserGjortComponent {
+export class LekserGjortComponent implements OnInit {
+  homeworkList: Lekse[];
 
+  constructor(lekseService: LekserService) {
+    this.homeworkList = lekseService.HentFullforte();
+  }
+
+
+  ngOnInit(): void {
+    // Perform initialization logic here
+    // For example, you can make an API call or initialize variables
+  }
 }
